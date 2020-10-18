50605286497c5211ee052e706436e7eb57944cd4
meTheLearner <togunchan@gmail.com>
2020-10-18 16:34:47 +0300

    1-Creating Form

---
+++ src/app/product/product.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../product.model';
+import { ProductRepository } from '../repository.model';
+
+@Component({
+  selector: 'app',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.css']
+})
+export class ProductComponent {
+
+  model: ProductRepository = new ProductRepository();
+
+  newProduct: Product = new Product();
+
+  get jsonProduct(){
+    return JSON.stringify(this.newProduct);
+  }
+
+  addProduct(p: Product){
+    console.log("New Product: "+ this.jsonProduct);
+  }
+
+  constructor() { }
+
+
+
+}
