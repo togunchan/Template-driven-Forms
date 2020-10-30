b7f52b933168d48b35cc7c1b5805dca6bac29061
meTheLearner <togunchan@gmail.com>
2020-10-30 18:06:47 +0300

    6-Validation Mesajlarının Metot ile Gösterilmesi

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -9,21 +9,47 @@ import { ProductRepository } from '../repository.model';
 })
 export class ProductComponent {
 
+  constructor() { }
+
   model: ProductRepository = new ProductRepository();
 
   newProduct: Product = new Product();
 
-  get jsonProduct(){
+  get jsonProduct() {
     return JSON.stringify(this.newProduct);
   }
 
-  addProduct(p: Product){
-    console.log("New Product: "+ this.jsonProduct);
+  addProduct(p: Product) {
+    console.log("New Product: " + this.jsonProduct);
+  }
+
+  log(x) {
+    console.log(x);
+  }
+
+  getValidationErrors(state: any) {
+    let ctrlName: string = state.name;
+    let messages: string[] = [];
+
+    if (state.errors) {
+      for (let errorName in state.errors) {
+        switch (errorName) {
+          case "required":
+            messages.push(`You must enter a ${ctrlName}`);
+            break;
+          case "minlength":
+            messages.push(`Min. 3 characters for ${ctrlName}`);
+            break;
+          case "pattern":
+            messages.push(`${ctrlName} contains illegal characters`);
+            break;
+        }
+      }
+    }
+    return messages;
   }
 
 
-
-  constructor() { }
 
 
 
